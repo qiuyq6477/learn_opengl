a8ddf1eea20e2ee00c8aeda89d013bb31f889a7b
qiu <819685619.com>
2021-03-15 12:05:43 +0800

    透视投影

--- learn_opengl/main.cpp
+++ learn_opengl/main.cpp
@@ -5,6 +5,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include "shader_s.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include <iostream>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -80,39 +83,21 @@ int main()
 	// texture 1
 	// ---------
 	glGenTextures(1, &texture1);
-	//绑定纹理，之后任何的纹理指令都可以配置当前绑定的纹理
 	glBindTexture(GL_TEXTURE_2D, texture1);
 	// set the texture wrapping parameters
-	// 为当前绑定的纹理对象设置环绕方式
-	//1，指定了纹理目标；我们使用的是2D纹理，因此纹理目标是GL_TEXTURE_2D
-	//2，指定设置的选项与应用的纹理轴，配置的是WRAP选项，并且指定S和T轴
-	//3，环绕方式(Wrapping)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // set texture  wrapping to GL_REPEAT (default wrapping method)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// set texture filtering parameters
-	//当进行放大(Magnify)和缩小(Minify)操作的时候可以设置纹理过滤的选项
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load image, create texture and generate mipmaps
-	//宽度、高度和颜色通道的个数
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded  texture's on the y-axis.
 	// The FileSystem::getPath(...) is part of the GitHub repository so we can find  files on any IDE/platform; replace it with your own image path.
 	unsigned char *data = stbi_load("./container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
-		//生成纹理
-		//1，指定了纹理目标(Target)。设置为GL_TEXTURE_2D意味着会生成与当前绑定的纹理对象在同一个目标上的纹理（任何绑定到GL_TEXTURE_1D和GL_TEXTURE_3D的纹理不会受到影响）。
-		//2，纹理指定多级渐远纹理的级别，填0，也就是基本级别。
-		//3，告诉OpenGL我们希望把纹理储存为何种格式
-		//4，设置宽度
-		//5，设置高度
-		//6，总是0
-		//7，源图的格式
-		//8，源图的数据类型
-		//9，真正的图像数据
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		//自动生成所有需要的多级渐远纹理。
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
@@ -147,11 +132,18 @@ int main()
 	//  -------------------------------------------------------------------------------------------
 	ourShader.use(); // don't forget to activate/use the shader before setting  uniforms!
 	// either set it manually like so:
-	//第一种方式，手动查询
 	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
 	// or set it via the texture class
-	//使用shader类，本质上和上面一样
 	ourShader.setInt("texture2", 1);
+	//变换矩阵，通过将顶点坐标乘以这个模型矩阵，我们将该顶点坐标变换到世界坐标。
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	//观察矩阵，opengl是右手坐标系，摄像机位于原点，
+	glm::mat4 view = glm::mat4(1.0f); // 注意，我们将矩阵向我们要进行移动场景的反方向移动。
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	//投影矩阵
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), (float)(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -163,14 +155,20 @@ int main()
 		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		// bind textures on corresponding texture units
-		// 在绑定纹理之前先激活纹理单元，在片段着色器中的位置
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		// render container
 		ourShader.use();
+		int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse  moved etc.)
