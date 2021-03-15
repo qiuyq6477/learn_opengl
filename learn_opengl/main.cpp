9a3de92ca40f0680e5f67e3214c82b0fc4602f8a
qiu <819685619.com>
2021-03-15 11:04:03 +0800

    创建相同的两个三角形，但对它们的数据使用不同的VAO和VBO

--- learn_opengl/main.cpp
+++ learn_opengl/main.cpp
@@ -110,12 +110,12 @@ int main()
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	// 定义顶点数据
-	float vertices[] = {
-		// first triangle
-	   -0.9f, -0.5f, 0.0f,  // left 
-	   -0.0f, -0.5f, 0.0f,  // right
-	   -0.45f, 0.5f, 0.0f,  // top 
-	   // second triangle
+	float firstTriangle[] = {
+		-0.9f, -0.5f, 0.0f,  // left 
+		-0.0f, -0.5f, 0.0f,  // right
+		-0.45f, 0.5f, 0.0f,  // top 
+	};
+	float secondTriangle[] = {
 		0.0f, -0.5f, 0.0f,  // left
 		0.9f, -0.5f, 0.0f,  // right
 		0.45f, 0.5f, 0.0f   // top 
@@ -124,24 +124,27 @@ int main()
 		   0, 1, 3,  // first Triangle
 		   1, 2, 3   // second Triangle
 	};
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO[2], VAO[2], EBO;
 	//创建顶点数组对象，任何随后的顶点属性调用都会储存在这个VAO中，
 	//这样的好处就是，当配置顶点属性指针时，你只需要将那些调用执行一次，之后再绘制物体的时候只需要绑定相应的VAO就行了。
 	//这使在不同顶点数据和属性配置之间切换变得非常简单，只需要绑定不同的VAO就行了。刚刚设置的所有状态都将存储在VAO中
 	//第一个参数是创建的数量，第二个是接收的地址
-	glGenVertexArrays(1, &VAO);
+	glGenVertexArrays(2, VAO);
 	//创建顶点缓冲对象，使用这些缓冲对象的好处是我们可以一次性的发送一大批数据到显卡上，而不是每个顶点发送一次。
-	glGenBuffers(1, &VBO);
+	glGenBuffers(2, VBO);
+
+	//========================================================
+
 	//创建索引缓冲对象，用来储存索引，OpenGL调用这些顶点的索引来决定该绘制哪个顶点。
 	//glGenBuffers(1, &EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and  then configure vertex attributes(s).
 	//绑定VAO，从绑定之后起，我们应该绑定和配置对应的VBO和属性指针，之后解绑VAO供之后使用。以后如果要绘制一个物体的时候，只要在绘制物体前简单地把VAO绑定到希望使用的设定上就行了。
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
 	//绑定缓冲，类型为GL_ARRAY_BUFFER
 	//OpenGL允许我们同时绑定多个缓冲，只要它们是不同的缓冲类型。
 	//从这一刻起，我们使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)。
 	//状态设置函数
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	//把顶点数据复制到顶点缓冲中
 	//1，目标缓冲的类型
 	//2，指定传输数据的大小(以字节为单位)
@@ -150,7 +153,7 @@ int main()
 	//* GL_STATIC_DRAW ：数据不会或几乎不会改变。
 	//* GL_DYNAMIC_DRAW：数据会被改变很多。
 	//* GL_STREAM_DRAW ：数据每次绘制时都会改变。
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
 	//绑定EBO
 	//复制索引数组到一个索引缓冲中
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -165,6 +168,15 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	//以顶点属性位置值作为参数，启用顶点属性。顶点属性默认是禁用的。
 	glEnableVertexAttribArray(0);
+
+	//========================================================
+
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as  the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	//这个时候是可以解绑VBO的，因为此时已经设定完顶点属性指针了
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -175,6 +187,7 @@ int main()
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind  VAOs (nor VBOs) when it's not directly necessary.
 	//此时可以解绑VAO了，
 	glBindVertexArray(0);
+
 	// uncomment this call to draw in wireframe polygons.
 	//开启线框模式
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -191,9 +204,13 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		// draw our first triangle
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no  need to bind it every time, but we'll do so to keep things a bit more organized
+		glBindVertexArray(VAO[0]); // seeing as we only have a single VAO there's no  need to bind it every time, but we'll do so to keep things a bit more organized
 		//如果没有使用EBO，就要用下面这句话来绘制三角形
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(VAO[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// glBindVertexArray(0); // no need to unbind it every time
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse  moved etc.)
@@ -203,9 +220,9 @@ int main()
 	}
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
+	//glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
