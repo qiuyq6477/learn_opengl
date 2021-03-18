f60e8efed7cea9c48f93399b1bea63a514387a7d
qiu <819685619.com>
2021-03-18 21:37:07 +0800

    让聚光灯的边缘更加平滑

--- learn_opengl/shader.fs
+++ learn_opengl/shader.fs
@@ -11,6 +11,7 @@ struct Light {
     vec3  position;
     vec3  direction;
     float cutOff;
+	float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
@@ -28,29 +29,26 @@ uniform vec3 viewPos;
 void main()
 {
 	vec3 lightDir = normalize(light.position - FragPos);
-	float theta = dot(lightDir, normalize(-light.direction));
-	if(theta > light.cutOff) 
-	{       
-	  // 执行光照计算
-	  // ambient
-		vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	float theta     = dot(lightDir, normalize(-light.direction));
+	float epsilon   = light.cutOff - light.outerCutOff;
+	float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);    
+	// ambient
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
   	
-		// diffuse 
-		vec3 norm = normalize(Normal);
-		vec3 lightDir = normalize(light.position - FragPos);
-		float diff = max(dot(norm, lightDir), 0.0);
-		vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
+	// diffuse 
+	vec3 norm = normalize(Normal);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
     
-		// specular
-		vec3 viewDir = normalize(viewPos - FragPos);
-		vec3 reflectDir = reflect(-lightDir, norm);  
-		float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-		vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));  
+	// specular
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));  
         
-		vec3 result = ambient + diffuse + specular;
-		FragColor = vec4(result, 1.0);
-	}
-	else  // 否则，使用环境光，让场景在聚光之外时不至于完全黑暗
-		FragColor= vec4(light.ambient * vec3(texture(material.diffuse, TexCoords)), 1.0);
-   
+	diffuse  *= intensity;
+	specular *= intensity;
+
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result, 1.0);
 } 
