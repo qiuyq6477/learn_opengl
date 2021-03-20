de8a34f35033ed1125850eb30518419605c2b1dd
qiu <819685619.com>
2021-03-20 15:47:36 +0800

    模糊效果

--- learn_opengl/shader_screen.fs
+++ learn_opengl/shader_screen.fs
@@ -21,11 +21,11 @@ void main()
         vec2( offset, -offset)  // сроб
     );
 
-    float kernel[9] = float[](
-        -1, -1, -1,
-        -1,  9, -1,
-        -1, -1, -1
-    );
+   float kernel[9] = float[](
+    1.0 / 16, 2.0 / 16, 1.0 / 16,
+    2.0 / 16, 4.0 / 16, 2.0 / 16,
+    1.0 / 16, 2.0 / 16, 1.0 / 16  
+);
 
     vec3 sampleTex[9];
     for(int i = 0; i < 9; i++)
